8719404f9ed5e7e91b54c6baf3e3cdd13b934ee3
C4COINteiner <gordonremotcontroler@gmail.com>
2015-01-20 20:09:40 +0100

    globals.hpp

--- globals.hpp
+++ globals.hpp
@@ -1,6 +1,6 @@
 #define iObsah 100
-#define iPatra 8    //min 2, max nevim, 13 trva 6 vterin, 7 pul
-#define screenX 960
+#define iPatra 2    //min 2, max nevim, 13 trva 6 vterin, 7 pul
+#define screenX 512
 #define screenY 0
-#define width 960
-#define height 1080
+#define width 512
+#define height 600
