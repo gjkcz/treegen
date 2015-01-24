6da68642dc9071dedb672b0f7d35676c723b05a0
NewJUser <jiroberf@seznam.cz>
2015-01-24 23:10:18 +0100

    drobne zprehledneni

--- Engine.hpp
+++ Engine.hpp
@@ -10,11 +10,9 @@
 #include <d3d9.h>
 #include <d3dx9.h>
 //#include <d3d11.h>
-//#include <d3dx9math.h>
 #include "SWU/Okno.hpp"
 #include "SWU/Konzole.hpp"
 #include "Kontroler3d.hpp"
-//#include "Input.hpp"
 #include "CustomVertex.hpp"
 #include "JednoduchyVertex.hpp"
 #include "TreeType.hpp"
@@ -36,16 +34,9 @@ public:
     void prectiVstupAUpravKameru();
     void render3d();
     void dejKonzoli(const sk::Konzole& x){iKonzole = x;}
-//    int* vemPocetVrcholuStromu() {return aPocetVrcholuStromu;}
-//    const LPDIRECT3DDEVICE9& vemG_pD3DDevice(){return g_pd3dDevice;}
-//    D3DXMATRIX* vemTreeMatrixy(){return treeMatrix;}
-//    LPDIRECT3DVERTEXBUFFER9* vemTreeVertexBuffers(){return treeVertexBuffers;}
-//    byte* vemKeys(){return iInput.Keys;}
-//    float* vemAxs(){return iInput.axs;}
 private:
     sw::Okno okno;
     sktrl::Kontroler3d iKontroler3d;
-//    si::Input iInput;
     sk::Konzole iKonzole;
     LPDIRECT3DDEVICE9 g_pd3dDevice; // Our rendering device, lp= neco pointer
     LPDIRECT3D9 g_pD3D; // Used to create the D3DDevice
@@ -56,7 +47,6 @@ private:
     float fFOV;
     D3DXMATRIX mScale, mRotateW;
     D3DXMATRIX maticeProjekce;
-
 
     static const bool bRust=false;
     float fDalka; //0
--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <windows.h>
-//#include <dinput.h>
 #include "Engine.hpp"
-//#include "view.hpp"
 #include "globals.hpp"
 
 int WINAPI WinMain (HINSTANCE hThisInstance,
@@ -13,9 +11,8 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
     sw::Pozice iPozice {screenX, screenY};
     sw::Rozmery iRozmery {width, height};
     sw::Okno iOkno(iPozice, iRozmery, "Tree");    //vytvor okno
-//    sw::Okno iOkno(sw::Pozice(512.f, screenY), sw::Rozmery(512, 726), "Tree");    //vytvor okno pro acer
+    //    sw::Okno iOkno(sw::Pozice(512.f, screenY), sw::Rozmery(512, 726), "Tree");    //vytvor okno pro acer
     se::Engine iEngine(iOkno);
-//    std::cout << "Ello!\n";
     iEngine.dejKonzoli(iKonzole);
     iEngine.priprav();  //vytvori si d3d a z nej g_pd3dDevice
     iOkno.ukaz();               // Show the window, az po inicializaci dx
@@ -24,12 +21,8 @@ int WINAPI WinMain (HINSTANCE hThisInstance,
         iEngine.prectiVstupAUpravKameru();
         if( iEngine.stav == WM_QUIT )
             break;
-
         iEngine.render3d();
-//        render( iEngine.vemG_pD3DDevice(), iEngine.vemPocetVrcholuStromu(),
-//                iEngine.vemKeys(), iEngine.vemAxs(), iEngine.vemTreeMatrixy(),
-//                iEngine.vemTreeVertexBuffers() );
     }
-//    system("pause");
+    //    system("pause");
     return 0;
 }
