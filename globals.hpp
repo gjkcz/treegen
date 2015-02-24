e1398df1a11667ac9e321c8c61ed47b656da7cfd
NewJUser <jiroberf@seznam.cz>
2015-02-24 18:36:20 +0100

    globals

--- globals.hpp
+++ globals.hpp
@@ -1,6 +1,15 @@
 #define iObsah 100
-#define iPatra 5    //min 2, max nevim, 13 trva 6 vterin, 7 pul
-#define screenX 512
+#define iPatra 7   //min 2, max nevim, 13 trva 6 vterin, 7 pul
+#define screenX 960
+//#define screenX 0
 #define screenY 0
-#define width 512
-#define height 600
+#define windowWidth 960
+//#define windowWidth 1920
+#define windowHeight 1080
+#define PI 3.14159265358979323846
+#define TREEVERBOSE
+//#define DESTRUCTORVERBOSE
+//#define DEBUG
+#define STACK
+//#define PROBLEMATICAL
+//#define OLDPROBABILITY
